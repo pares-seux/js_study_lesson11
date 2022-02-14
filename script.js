e7eb1a7a4bf2f9307c4fe1443ca5fcb5391c29ed
pares-seux <paresseux.992@gmail.com>
2022-02-14 09:38:21 +0400

    lesson-11

---
+++ script.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector("#btn");
+const input = document.querySelector("input[type=text]");
+const square = document.querySelector("#square");
+const insideButton = document.querySelector("#e_btn");
+const range = document.querySelector("#range");
+const rangeSpan = document.querySelector("#range-span");
+const circle = document.querySelector("#circle");
+
+const radiusCircle = function(event) {
+  rangeSpan.textContent = event.target.value + "%";
+  if (event.type === "change") {
+    circle.style = "width: " + event.target.value + "%; height: " + event.target.value + "%;"
+  }
+};
+
+btn.addEventListener("click", function () {
+  if (isNaN(input.value.trim())) {
+    square.style.backgroundColor = input.value;
+    input.value = "";
+  }
+});
+
+insideButton.addEventListener("click", function () {
+  insideButton.style.display = "none";
+});
+
+range.addEventListener("input", radiusCircle);
+range.addEventListener("change", radiusCircle);
